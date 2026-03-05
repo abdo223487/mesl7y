e76a7b038271107f1fb94d70d4817b35d8b63673
Abdallah Ibrahim <abdallahelhdad223@gmail.com>
2026-03-05 04:16:49 +0200

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,7 @@
+import 'package:my_mesl7y_app_server/server.dart';
+
+/// This is the starting point for your Serverpod server. Typically, there is
+/// no need to modify this file.
+void main(List<String> args) {
+  run(args);
+}
